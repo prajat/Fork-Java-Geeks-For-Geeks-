518a06d530ff95a9d29230640e06a0dfa3cc1d6b
prajat <rajat.singh1084@gmail.com>
2018-12-05 23:47:12 +0530

    ques solved

---
+++ MODULE-1/logicaloperator.java
@@ -0,0 +1,7 @@
+class Geeks {
+
+    static void logicOp(boolean a, boolean b) {
+        /* output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces */
+        System.out.println((a && b) + " " + (a || b) + " " + ((!a) && (!b)));
+    }
+}
