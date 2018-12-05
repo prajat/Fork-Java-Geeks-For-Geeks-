9bfb65db1c9e08301b29e1382d2095e283c95cdd
prajat <rajat.singh1084@gmail.com>
2018-12-05 23:41:49 +0530

    correction

---
+++ MODULE-1/learntocomment.java
@@ -0,0 +1,11 @@
+class Geeks {
+
+    // Function to print a and c
+    // comment the line that outputs b
+    static void comment(int a, int b, int c) {
+        System.out.println(a);
+        // System.out.println(b);
+        System.out.println(c);
+    }
+
+}
