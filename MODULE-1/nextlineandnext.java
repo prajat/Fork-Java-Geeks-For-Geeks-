b4995544bf1f1e74265847f5a9ed1c403eb7396a
prajat <rajat.singh1084@gmail.com>
2018-12-05 23:25:15 +0530

    solved

---
+++ MODULE-1/nextlineandnext.java
@@ -0,0 +1,18 @@
+class Geeks {
+    static void getInput() {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Taking the number of testcases
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            String s = sc.nextLine();
+            s = sc.nextLine();
+
+            // Your code here
+
+            System.out.println(a);
+            System.out.println(s);
+        }
+
+    }
+}
