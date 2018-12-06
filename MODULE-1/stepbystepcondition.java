2404691ea7c6a228a0afdf7e844ae6b87f2d3f9b
prajat <rajat.singh1084@gmail.com>
2018-12-06 23:46:31 +0530

    ques solved

--- MODULE-1/stepbystepcondition.java
+++ MODULE-1/stepbystepcondition.java
@@ -1,21 +1,25 @@
 class Geeks {
-    static void isDivisibleByPrime(int n) {
-        // Your code here
-        if (n % 11 == 0) {
-            System.out.println("ELEVEN");
-        }
-        else if(n%3==0){
-            System.out.println("THREE");
-        }
-        else if (n % 2 == 0) {
-            System.out.println("TWO");
+    static void isPrime(int n) {
+        int flag = 1;
+        if (n == 1) {
+            System.out.println("No");
+        } else {
 
-        }
-        else {
-            System.out.println("-1");
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                // Your code here
+                if (n % i == 0) {
+                    flag = 0;
 
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println("No");
+            } else {
+                System.out.println("Yes");
+            }
         }
 
-        
+        // System.out.println();
     }
 }
