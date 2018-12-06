1c80210e607aaf9c89cba3ce4259f95a24c91c08
prajat <rajat.singh1084@gmail.com>
2018-12-06 23:59:51 +0530

    corrections

--- MODULE-1/takinginput.java
+++ MODULE-1/takinginput.java
@@ -1,38 +1,28 @@
 import java.util.Scanner;
 
 class takinginput {
-    public static void main(String[] args) {
-        int x = 0;
-        int y = 0;
-        for (int z = 0; z < 5; z++) {
-            if ((++x > 2) || (++y > 2)) {
-                x++;
-            }
-        }
-        System.out.println(x + " " + y);
-    }
-
+    
     // Function to take input using Scanner class
     static void IOFunction() {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
-        // while (t-- > 0) {
+        while (t-- > 0) {
 
-        //     // Your code here
-        //     int a = sc.nextInt();
-        //     float b = sc.nextFloat();
-        //     long c = sc.nextLong();
-        //     byte d = sc.nextByte();
-        //     String s = sc.nextLine();
+            // Your code here
+            int a = sc.nextInt();
+            float b = sc.nextFloat();
+            long c = sc.nextLong();
+            byte d = sc.nextByte();
+            String s = sc.nextLine();
 
-        //     System.out.println(a);
-        //     System.out.println(b);
-        //     System.out.println(c);
-        //     System.out.println(d);
-        //     System.out.println(s);
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(s);
 
-        // }
+        }
         
                 
             }
