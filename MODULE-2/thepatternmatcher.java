cc743bfdb1136dcd6096b732b5ea43b4f7ad7fa9
prajat <rajat.singh1084@gmail.com>
2018-12-09 00:04:54 +0530

    ques on hold

---
+++ MODULE-2/thepatternmatcher.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+class Geeks {
+
+    static void follPatt(String s) {
+
+        // Your code here
+    //     HashMap<Character, Integer> map = new HashMap<>();
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char ch = s.charAt(i);
+    //         if (map.containsKey(ch)) {
+    //             map.put(ch, map.get(ch) + 1);
+    //         } else {
+    //             map.put(ch, 1);
+    //         }
+    //     }
+    //     int ans = 0;
+    //     if (map.get('x') == map.get('y')) {
+    //         ans = 1;
+    //     }
+    //     System.out.println(ans);
+        
+
+    //     System.out.println();
+    // }
+    Stack<Integer> stack=new Stack<>();
+    for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(ch=='x'){
+            stack.push(1);
+
+        }
+        if(ch=='y'){
+            stack.pop();
+        }
+    }
+    if(stack.isEmpty()){
+        System.out.println(1);
+    }
+    else{
+        System.out.println(0);
+    }
+}
