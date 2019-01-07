115d6c03b8ae3fcee30d8316a86fd825cf96310d
prajat <rajat.singh1084@gmail.com>
2019-01-07 22:12:06 +0530

    ques solved

--- MODULE-2/xoringandclearing.java
+++ MODULE-2/xoringandclearing.java
@@ -7,4 +7,29 @@ public class xoringandclearing {
     public static void main(String[] args) {
         
     }
+    
+    static void printArr(int arr[], int n) {
+        // Your code for printing arr here
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println();
+    }
+
+    static void setToZero(int arr[], int n) {
+        // use Arrays.fill to set arr to zero
+
+        Arrays.fill(arr, 0);
+    }
+
+    static void xor1ToN(int arr[], int n) {
+
+        // Xor arr[i]^i
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] ^ i;
+        }
+
+    }
 }
