6e360f26fc3fd5af7a99a0578bd0abc23c936663
prajat <rajat.singh1084@gmail.com>
2019-01-08 23:05:49 +0530

    ques solved

---
+++ contest-3/areaofcuboid.java
@@ -0,0 +1,11 @@
+class Geeks// Write correct class name here
+{
+    static void area(Cuboid c) // Correct name and parameters here
+    {
+        // Your code here
+
+        int ans = c.l * c.b * c.h;
+        System.out.println(ans);
+
+    }
+}
