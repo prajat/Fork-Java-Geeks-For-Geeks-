f316e436f68c7ffe1319520d099ae68dbc8f9e42
prajat <rajat.singh1084@gmail.com>
2019-01-08 22:48:50 +0530

    ques solved

---
+++ MODULE-3/sumofbigintegers.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+class MathematicalOperation {
+
+    static BigInteger add(BigInteger x, BigInteger y) {
+
+        // Your code here
+        BigInteger z = x.add(y);
+        System.out.println(z);
+
+    }
+
+}
