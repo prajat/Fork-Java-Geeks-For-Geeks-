6355c154a6c22f07e3ca01f3406a65670da95ea5
prajat <rajat.singh1084@gmail.com>
2019-01-08 22:50:27 +0530

    ques solved

---
+++ MODULE-3/bigintegermultiply.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+class MathematicalOperation {
+
+    static BigInteger mul(BigInteger x, BigInteger y) {
+
+        // Your code here
+        BigInteger z = x.multiply(y);
+        return z;
+
+    }
+
+}
