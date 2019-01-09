0b04827872905dc0dc824c80861494b5784545b1
prajat <rajat.singh1084@gmail.com>
2019-01-09 23:23:28 +0530

    ques solved

--- MODULE-5/sumthecommonelements.java
+++ MODULE-5/sumthecommonelements.java
@@ -8,11 +8,11 @@ class Geeks {
 
         HashSet<Integer> set1 = new HashSet<>();
         for (int i = 0; i < arr1.length; i++) {
-            set1.add(i);
+            set1.add(arr1[i]);
         }
         HashSet<Integer> set2 = new HashSet<>();
         for (int i = 0; i < arr2.length; i++) {
-            set2.add(i);
+            set2.add(arr2[i]);
         }
         long sum = 0;
 
