00593a1f9ca7f82306510ba0e4436a3ea46f19b6
prajat <rajat.singh1084@gmail.com>
2019-01-09 21:55:07 +0530

    ques solved

---
+++ MODULE-5/sumthecommonelements.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+class Geeks {
+    public static long commonSum(int arr1[], int arr2[]) {
+        // Your code here
+
+        HashSet<Integer> set1 = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set1.add(i);
+        }
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < arr2.length; i++) {
+            set2.add(i);
+        }
+        long sum = 0;
+
+        for (Integer i : set2) {
+            if (set1.contains(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
